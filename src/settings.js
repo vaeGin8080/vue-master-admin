6f55659eae41add13eddbebc1506af73d53fbc62
vaeGin8080 <1379202781@qq.com>
2021-01-18 16:41:18 +0800

    vue-admin-templete 首次提交

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '萌宠集中营',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+  fixedHeader: true
+}
---
+++ src/views/tinymce/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="components-container">
+    <div>
+      <tinymce v-model="content" :height="300" />
+    </div>
+    <div class="editor-content" v-html="content" />
+    <el-button type="primary" @click="submit">提交</el-button>
+  </div>
+</template>
+
+<script>
+import Tinymce from '@/components/Tinymce'
+
+export default {
+  name: 'TinymceDemo',
+  components: { Tinymce },
+  data() {
+    return {
+      content:  ``
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.content)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.editor-content{
+  margin-top: 20px;
+}
+</style>
+
